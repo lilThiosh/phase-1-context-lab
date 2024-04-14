764ae288fb91b4625e649f7d8f23ab1660eef777
Ian <user.emai>
2024-04-14 23:40:25 +0300

    Updating our time-card and payroll application to use the employee record as context rather than passing it as an argument.

--- index.js
+++ index.js
@@ -20,4 +20,56 @@ const allWagesFor = function () {
 
     return payable
 }
+const createEmployeeRecord = function (arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+};
 
+const createEmployeeRecords = function (arr) {
+    return arr.map(createEmployeeRecord);
+};
+
+const createTimeInEvent = function (dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this;
+};
+
+const createTimeOutEvent = function (dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this;
+};
+
+const hoursWorkedOnDate = function (date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    const hoursWorked = (timeOut.hour - timeIn.hour) / 100;
+    return hoursWorked;
+};
+
+const wagesEarnedOnDate = function (date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+};
+const findEmployeeByFirstName = function (srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+};
+
+const calculatePayroll = function (arr) {
+    return arr.reduce((totalPay, employee) => totalPay + allWagesFor.call(employee), 0);
+};
